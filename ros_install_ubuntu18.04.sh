6b92c74e8ede842f601ef1aefc708238aa439329
Longer <77575048+neomakers@users.noreply.github.com>
2023-09-15 13:12:51 +0800

    Update ros_install_ubuntu18.04.sh

--- ros_install_ubuntu18.04.sh
+++ ros_install_ubuntu18.04.sh
@@ -36,9 +36,9 @@ echo "adding the ROS repository to your ubuntu sources.list"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 echo "add the keys for accessing "
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-keyiC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key5523BAEEB01FA116
 
-  update_package_manager
+  update_package_manager  
 # 在这里指定要安装的包名称
   package_to_install="ros-melodic-desktop-full"
   install_package "$package_to_install"
