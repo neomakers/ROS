a848d213c4ec8cda23e91640138df960aee42ac4
Longer <77575048+neomakers@users.noreply.github.com>
2023-09-15 13:46:21 +0800

    Update ros_install_ubuntu18.04.sh

--- ros_install_ubuntu18.04.sh
+++ ros_install_ubuntu18.04.sh
@@ -36,6 +36,8 @@ echo "adding the ROS repository to your ubuntu sources.list"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 echo "add the keys for accessing "
+
+#一下代码参考来自https://answers.ros.org/question/325039/apt-update-fails-cannot-install-pkgs-key-not-working/
 sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
   update_package_manager  
